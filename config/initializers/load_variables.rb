eefa4b4bb5c60f7f29efebc98492688cb4286fd5
Rod Wilhelmy <rwilhelmy@gmail.com>
2012-12-01 17:07:14 -0600

    Initializer will look up for variables in the environment first

--- config/initializers/load_variables.rb
+++ config/initializers/load_variables.rb
@@ -1,4 +1,4 @@
 env = YAML.load_file("#{Rails.root}/config/environment_variables.yml")[Rails.env]
-GITHUB_KEY = env['github']['key']
-GITHUB_SECRET = env['github']['secret']
+GITHUB_KEY = ENV['GITHUB_KEY'] || env['github']['key']
+GITHUB_SECRET = ENV['GITHUB_SECRET'] || env['github']['secret']
 
