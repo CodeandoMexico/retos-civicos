32c4915cb5c684d9e8265c82379c7ef490960405
Juan Pablo Escobar <juannpablo@gmail.com>
2012-12-01 13:17:32 -0600

    First commit for Aquila project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aquila::Application.config.secret_token = 'af21de11511c9306be4447209c8089d270455977fa1565498a17417218e72e121d491269472ed55fd74780252b71c7ae8dc770b9782d4e97a5da79c0b7ea569f'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Aquila::Application.routes.draw do
+
+  root :to => 'home#index'
+
+end
