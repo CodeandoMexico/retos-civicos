da198ea7dda306671bc489ff11ba3c6cd18e58ce
edolopez <elopez.deleon@gmail.com>
2013-04-03 09:01:29 -0600

    i18n implemented in all controllers

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -8,7 +8,7 @@ class AuthenticationsController < ApplicationController
       sign_in_and_redirect(authentication.user_id)
     elsif current_user
       current_user.authentications.create_with_omniauth(provider: omniauth['provider'], uid: omniauth['uid'])
-      redirect_back_or root_url, "New authentication successful."
+      redirect_back_or root_url, t('auth_controller.new_auth')
     else
       user = User.create_with_omniauth(omniauth)
       user.authentications.first.update_attribute(:public_url, omniauth.info.urls.public_profile) if omniauth.provider == "linkedin" && !user.authentications.first.public_url.present?
@@ -18,18 +18,18 @@ class AuthenticationsController < ApplicationController
 
   def session_destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Has salido satisfactoriamente"
+    redirect_to root_url, notice: t('auth_controller.destroy')
   end
 
   def failure
-    redirect_to root_path, notice: "Acceso no autorizado, intenta de nuevo"
+    redirect_to root_path, notice: t('auth_controller.failure')
   end
 
   private
 
   def sign_in_and_redirect(user_id)
     session[:user_id] = user_id
-    redirect_back_or projects_path, "Bienvenido a la plataforma"
+    redirect_back_or projects_path, t('auth_controller.sign_in')
   end
 
 end
