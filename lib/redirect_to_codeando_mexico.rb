1d6bbf47c060c92fd795e57ad61a0001f0528f94
Abraham Kuri <kurenn@icalialabs.com>
2013-06-28 11:21:32 -0500

    Added complete pattern for replacing the heroku url

--- lib/redirect_to_codeando_mexico.rb
+++ lib/redirect_to_codeando_mexico.rb
@@ -8,7 +8,7 @@ class RedirectToCodeandoMexico
   def call(env)
     if env['HTTP_HOST'].include?(APP_NAME)
       request_url = Rack::Request.new(env).url
-      redirect_to_url = request_url.gsub(/codeando-mexico/, "codendomexico.org")
+      redirect_to_url = request_url.gsub(/codeando-mexico.herokuapp.com|codeando-mexico.heroku.com/, "codendomexico.org")
       [301, { 'Location' => redirect_to_url }, ['Redirecting...']]
     else
       @app.call(env)
