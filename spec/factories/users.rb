a3b5369c145a26b857d31f0f40c9f1fc31e93164
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-16 13:38:33 -0600

    start user test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name "nombre"
-    email "correo@codeandomexico.org"
+    sequence(:email) {|n| "correo#{n}@codeandomexico.org" }
     nickname "cmx"
     password "password"
     password_confirmation "password"
