5f26449d84ecc9f62a5da0bdd01c71897e259c88
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-16 12:34:04 -0600

    remove test folder

--- test/functional/organizations_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OrganizationsControllerTest < ActionController::TestCase
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/collaborations_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CollaborationsHelperTest < ActionView::TestCase
-end
