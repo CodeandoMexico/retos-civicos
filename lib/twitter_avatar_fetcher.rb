12487a8d6eb5e40c96e5912b840a5f1357f2beec
Adrian Rangel <adrian.rangel@gmail.com>
2014-04-10 13:59:06 -0500

    [Issue #347] Actualizar gema de Twitter

--- lib/twitter_avatar_fetcher.rb
+++ lib/twitter_avatar_fetcher.rb
@@ -2,6 +2,12 @@ class TwitterAvatarFetcher
 
   def initialize(user_id)
     @user_id = user_id
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = TWITTER_KEY
+      config.consumer_secret = TWITTER_SECRET
+      config.access_token = TWITTER_ACCESS_KEY
+      config.access_token_secret = TWITTER_ACCESS_SECRET
+    end
   end
 
   def user
@@ -13,7 +19,7 @@ class TwitterAvatarFetcher
   end
 
   def fetch
-    image_url = Twitter.user(auth.uid.to_i).profile_image_url.sub("_normal", "")
+    image_url = @client.user(auth.uid.to_i).profile_image_url.to_s.sub("_normal", "")
 
     user.remote_avatar_url = image_url
     user.save validate: false
