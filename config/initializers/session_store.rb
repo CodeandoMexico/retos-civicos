dc6cffb3fed0e343b120f1a4d4b5d971e410e7a8
bhserna <bhserna@gmail.com>
2014-04-21 17:50:13 -0500

    fixes session bug with different subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Aquila::Application.config.session_store :cookie_store, key: '_aquila_session'
+Aquila::Application.config.session_store :cookie_store, key: '_aquila_session', domain: :all, tld_length: 2
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
