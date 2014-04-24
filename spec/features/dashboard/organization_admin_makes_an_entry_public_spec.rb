b33ecbeed75b70d054ba4938643a1e6fa22feac3
bhserna <bhserna@gmail.com>
2014-04-24 16:59:50 -0500

    organization admin can make an entry public from the entry page

--- spec/features/dashboard/organization_admin_makes_an_entry_public_spec.rb
+++ spec/features/dashboard/organization_admin_makes_an_entry_public_spec.rb
@@ -1,23 +1,36 @@
 require 'spec_helper'
 
 feature 'Organization admin makes an entry public' do
-  attr_reader :member, :organization, :organization_admin
+  attr_reader :member, :organization, :organization_admin, :challenge, :entry
 
   before do
     user = create :user, name: 'Juanito'
     @member = create :member, user: user
     @organization = create :organization, subdomain: 'superorg'
     @organization_admin = create :user, userable: organization
+    @challenge = create :challenge, title: 'Reto 1', organization: organization
+    @entry = create :entry, name: 'Mi propuesta', challenge: challenge, member: member
   end
 
   scenario 'in the entries list' do
-    challenge = create :challenge, title: 'Reto 1', organization: organization
-    entry = create :entry, challenge: challenge, member: member
-
     visit challenge_entry_path(challenge, entry)
     page.status_code.should eq 404
+
     sign_in_organization_admin(organization_admin)
     click_link 'Propuestas'
+
+    click_button 'Hacer pública'
+    click_link 'Ver vista pública'
+
+    page.should have_content entry.name
+    current_path.should eq challenge_entry_path(challenge, entry)
+  end
+
+  scenario 'in the entry page' do
+    sign_in_organization_admin(organization_admin)
+    click_link 'Propuestas'
+    click_link 'Mi propuesta'
+
     click_button 'Hacer pública'
     click_link 'Ver vista pública'
 
