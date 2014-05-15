cbbba3ac6c2707e10659096e3a168de151dbdac4
bhserna <bhserna@gmail.com>
2014-05-15 12:30:59 -0500

    organization admin can update organization profile

---
+++ spec/features/dashboard/organization_admin_updates_organization_profile_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'Organization admin updates organization profile' do
+  scenario 'from the dashboard' do
+    organization = create :organization
+    sign_in_organization_admin(organization.admin)
+
+    click_on 'Configuración'
+    fill_in 'organization_name', with: 'Updated org name'
+    click_button 'Actualizar'
+
+    current_path.should eq dashboard_path
+    dashboard_should_show_the_new_name('Updated org name')
+  end
+
+  def dashboard_should_show_the_new_name(new_name)
+    page.should have_content new_name
+  end
+end
