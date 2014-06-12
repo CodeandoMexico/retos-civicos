9a28d62565945daf1957bbc64c8de896a0bd7881
bhserna <bhserna@gmail.com>
2014-06-12 16:02:43 -0500

    fix redirection to app url

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module Aquila
     # Prevent initializing the app during assets precompilation
     config.assets.initialize_on_precompile = false
 
-    config.middleware.use "RedirectToCodeandoMexico" if Rails.env.production?
+    config.middleware.use "RedirectToAppUrl" if Rails.env.production?
 
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
     config.assets.precompile += %w( dashboard.css )
