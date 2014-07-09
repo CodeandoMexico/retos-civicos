a5f55bceec76b6ec1306cf05828cc9022dffdcd7
bhserna <bhserna@gmail.com>
2014-07-09 14:19:20 -0500

    adds a fifth spec to challenges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140709184823) do
+ActiveRecord::Schema.define(:version => 20140709191332) do
 
   create_table "activities", :force => true do |t|
     t.text     "text"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(:version => 20140709184823) do
     t.date     "prototypes_phase_due_on"
     t.date     "starts_on"
     t.text     "fourth_spec"
+    t.text     "fifth_spec"
   end
 
   add_index "challenges", ["organization_id"], :name => "index_challenges_on_organization_id"
