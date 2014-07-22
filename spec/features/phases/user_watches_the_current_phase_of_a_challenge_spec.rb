130f7b8146e62f38a9d70218c6f6176a44acfc3b
Raúl Jiménez <jimenezjrs@gmail.com>
2014-07-22 12:06:01 -0500

    added a message banner for the challenges' last day

--- spec/features/phases/user_watches_the_current_phase_of_a_challenge_spec.rb
+++ spec/features/phases/user_watches_the_current_phase_of_a_challenge_spec.rb
@@ -10,6 +10,16 @@ feature 'User watches the current phase of a challenge' do
     page_should_show_completness_of_ideas_phase('30.0')
   end
 
+  scenario 'when the current phase is the ideas phase with zero days left' do
+    challenge = create :challenge,
+      starts_on: 3.days.ago,
+      ideas_phase_due_on: 0.days.from_now.to_date
+
+    visit challenge_path(challenge)
+    page_should_show_completness_of_ideas_phase('100.0')
+    page.should have_content('Último día')
+  end
+
   def page_should_show_completness_of_ideas_phase(percentage)
     within ".phases-bar-phase:nth-child(2)" do
       page.should have_css "[data-width='#{percentage}']"
