ad303aaf3630b144d0c208fb82744e86e7554e1b
Raúl Jiménez <jimenezjrs@gmail.com>
2014-08-07 21:59:28 -0500

    fix for an issue with dates

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -43,7 +43,7 @@ class Challenge < ActiveRecord::Base
   }
 
   scope :recent, lambda {
-    where(['starts_on <= ?', DateTime.now]).order('created_at DESC')
+    where(['starts_on <= ?', Date.current]).order('created_at DESC')
   }
 
   scope :popular, lambda {
