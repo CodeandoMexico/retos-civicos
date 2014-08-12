062c661920f60e9e1edcbb7436526eb0bf1c6014
Raúl Jiménez <jimenezjrs@gmail.com>
2014-08-11 19:46:02 -0500

    fixes challenge messages when it has finished or it hasn't started

---
+++ spec/features/phases/user_watches_a_null_phase_challenge.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+feature "When a challenge has a null phase" do
+ attr_reader :finished_challenge, :unstarted_challenge
+
+    before do
+      @finished_challenge = create :challenge,
+        ideas_phase_due_on: 2.weeks.ago,
+        ideas_selection_phase_due_on: 1.week.ago,
+        prototypes_phase_due_on: 1.week.ago,
+        finish_on: 2.days.ago
+
+      @unstarted_challenge = create :challenge,
+        starts_on: 3.days.from_now,
+        ideas_phase_due_on: 2.weeks.from_now,
+        ideas_selection_phase_due_on: 1.week.from_now,
+        prototypes_phase_due_on: 1.week.from_now,
+        finish_on: 2.days.from_now
+    end
+
+    scenario 'and is finished it should say it has concluded' do
+      visit challenge_path(finished_challenge)
+      page.should have_content "Concluido"
+    end
+
+    scenario 'and is finished it should say it has concluded' do
+      visit challenge_path(unstarted_challenge)
+      page.should have_content "No ha comenzado"
+    end
+end
