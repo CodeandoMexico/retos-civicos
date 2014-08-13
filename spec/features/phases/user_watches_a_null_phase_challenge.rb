7f6e961bd0204e6bbd507d20fa5f248e52eb631d
Raúl Jiménez <jimenezjrs@gmail.com>
2014-08-13 11:29:21 -0500

    added a test that tests today

--- spec/features/phases/user_watches_a_null_phase_challenge.rb
+++ spec/features/phases/user_watches_a_null_phase_challenge.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature "When a challenge has a null phase" do
- attr_reader :finished_challenge, :unstarted_challenge
+    attr_reader :finished_challenge, :unstarted_challenge, :starts_today_challenge
 
     before do
       @finished_challenge = create :challenge,
@@ -11,7 +11,14 @@ feature "When a challenge has a null phase" do
         finish_on: 2.days.ago
 
       @unstarted_challenge = create :challenge,
-        starts_on: 3.days.from_now,
+        starts_on: 1.day.from_now,
+        ideas_phase_due_on: 2.weeks.from_now,
+        ideas_selection_phase_due_on: 1.week.from_now,
+        prototypes_phase_due_on: 1.week.from_now,
+        finish_on: 2.days.from_now
+
+      @starts_today_challenge = create :challenge,
+        starts_on: Date.current,
         ideas_phase_due_on: 2.weeks.from_now,
         ideas_selection_phase_due_on: 1.week.from_now,
         prototypes_phase_due_on: 1.week.from_now,
@@ -26,5 +33,11 @@ feature "When a challenge has a null phase" do
     scenario 'and is finished it should say it has concluded' do
       visit challenge_path(unstarted_challenge)
       page.should have_content "No ha comenzado"
+    end
+
+    scenario 'and has started it should say it has started' do
+      visit challenge_path(starts_today_challenge)
+      save_and_open_page
+      page.should have_content "Ideas"
     end
 end
