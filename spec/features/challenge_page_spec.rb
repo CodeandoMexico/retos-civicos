67e573ae2b91a628c1cab93c9627ff14ffc5ae55
Adrian Rangel <adrian.rangel@gmail.com>
2014-09-11 18:23:32 -0500

    create scenario for assessment methodology button

--- spec/features/challenge_page_spec.rb
+++ spec/features/challenge_page_spec.rb
@@ -9,6 +9,28 @@ feature "Commenting on challenge" do
     sign_in_user(member.user, password: 'password')
   end
 
+  scenario "show 'Metodologia de evaluacion' button when phase is prototype" do
+    challenge_prototype = create :challenge,
+      organization: organization,
+      title: 'Limpiemos México',
+      pitch: 'Hagamos conciencia para un México limpio',
+      entry_template_url: 'http://google.com',
+      assessment_methodology: 'http://google.com',
+      description: 'México esta muy sucio',
+      starts_on: 5.days.ago.to_date,
+      ideas_phase_due_on: 4.days.ago.to_date,
+      ideas_selection_phase_due_on: 3.days.ago.to_date,
+      prototypes_phase_due_on: 3.month.from_now.to_date,
+      finish_on: 4.month.from_now.to_date
+    entry = create :entry,
+      accepted: true,
+      challenge: challenge_prototype,
+      member: member
+    visit challenge_path(challenge_prototype)
+
+    find_link('Metodología de Evaluación')
+  end
+
   scenario "Can reply to a comment", js: true do
     create :comment, commentable: challenge
     # Clear emails from suscriptions
