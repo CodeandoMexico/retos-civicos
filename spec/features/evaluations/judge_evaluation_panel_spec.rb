2b4f27db7405b223f72c28e6d745434cb22378c9
Raúl Jiménez <jimenezjrs@gmail.com>
2014-11-19 19:05:59 -0600

    added tests for adding a new judge

--- spec/features/evaluations/judge_evaluation_panel_spec.rb
+++ spec/features/evaluations/judge_evaluation_panel_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 feature 'Judge' do
   attr_reader :judge_with_no_evaluations, :judge_with_evaluations, :challenge_without_evaluations, :challenge_with_evaluations, :evaluation
-<<<<<<< HEAD
-
-=======
->>>>>>> finished doing the text and did some minor refactoring
   before do
     @judge_with_no_evaluations = create :judge
     @judge_with_evaluations = create :judge
@@ -24,7 +20,6 @@ feature 'Judge' do
     visit_evaluation_panel
 
     expect(page).to have_content "No tienes retos asignados"
-<<<<<<< HEAD
   end
 
   scenario 'visits evaluation panel and there is a challenge to evaluate' do
@@ -47,38 +42,11 @@ feature 'Judge' do
   end
 
   scenario 'submits an invalid (pdf) evaluation file for a challenge' do
-=======
-  end
-
-  scenario 'visits evaluation panel and there is a challenge to evaluate' do
-    sign_in_user judge_with_evaluations
-    visit_evaluation_panel
-
-    expect(page).to have_content "Challenge With Evaluations"
-  end
-
-
-  scenario 'submits an valid (csv) evaluation file for a challenge' do
->>>>>>> finished doing the text and did some minor refactoring
     sign_in_user judge_with_evaluations
     visit_evaluation_panel
 
     submit_a_evaluation_file_with(
       challenge_title: challenge_with_evaluations.title,
-<<<<<<< HEAD
-=======
-      evaluation_file: evaluation_csv_file
-    )
-    expect(current_path).to eq evaluations_path
-  end
-
-  scenario 'submits an invalid (pdf) evaluation file for a challenge' do
-    sign_in_user judge_with_evaluations
-    visit_evaluation_panel
-
-    submit_a_evaluation_file_with(
-      challenge_title: challenge_with_evaluations.title,
->>>>>>> finished doing the text and did some minor refactoring
       evaluation_file: evaluation_pdf_file
     )
     expect(current_path).to eq evaluation_path(evaluation)
