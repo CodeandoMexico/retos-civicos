b655f65ee278f3cc3519ce32c5c1292f2abfd161
Raúl Jiménez <jimenezjrs@gmail.com>
2015-09-08 19:45:02 -0500

    removed temporary the facebook provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :confirmable,
     :recoverable, :validatable
-  devise :omniauthable, omniauth_providers: [:facebook, :twitter]
+  # devise :omniauthable, omniauth_providers: [:facebook, :twitter]
+  devise :omniauthable, omniauth_providers: [:twitter]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
