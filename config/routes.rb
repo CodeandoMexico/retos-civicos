723119169594d0e53b47f232a1714d36e4156f48
Raúl Jiménez <jimenezjrs@gmail.com>
2015-09-10 11:09:50 -0500

    minor changes to unused routes and privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -110,9 +110,9 @@ Aquila::Application.routes.draw do
   end
 
   match "/set_language" => 'pages#set_language', via: :post, as: 'set_language'
-  match "/terms_of_service" => 'pages#terms_of_service', via: :get, as: 'terms_of_service'
+  # match "/terms_of_service" => 'pages#terms_of_service', via: :get, as: 'terms_of_service'
   match "/privacy" => 'pages#privacy', via: :get, as: 'privacy'
-  get "/about", to: "pages#about", as: "about"
+  # get "/about", to: "pages#about", as: "about"
   get "/start_a_challenge", to: "pages#start_a_challenge", as: "start_a_challenge"
 
   root :to => 'challenges#index'
