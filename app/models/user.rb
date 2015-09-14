e2b71941fb60a58c1b80ad8d62ec7e6a9ba614bb
Raúl Jiménez <jimenezjrs@gmail.com>
2015-09-14 13:55:12 -0500

    added facebook provider to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :confirmable,
     :recoverable, :validatable
-  # devise :omniauthable, omniauth_providers: [:facebook, :twitter]
-  devise :omniauthable, omniauth_providers: [:twitter]
+  devise :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
