83df1e8d60b84bb7e6b156ea62c027eb338d61fe
jakegilger <jgilger@berkeley.edu>
2016-03-18 06:22:07 +0000

    added some tests

--- spec/features/brigade_spec.rb
+++ spec/features/brigade_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 feature 'Brigades' do
-  scenario 'Creating a Brigade as a logged in user' do
+  let!(:signedin) do
     user = new_user
     sign_in_user(user)
+  end
+  
+  scenario 'Creating a Brigade as a logged in user' do
     visit new_brigade_en_path
     expect(page).to have_content I18n.t('brigades.new.header')
     expect(page).to have_content I18n.t('brigades.new.submit')
@@ -11,5 +14,29 @@ feature 'Brigades' do
     fill_in 'brigade_description', with: 'Bienvenido a la brigada de Monterrey!'
     click_on I18n.t('brigades.new.submit')
     expect(page).to have_content 'Monterrey'
+  end
+  
+  scenario 'Follow a Brigade as a logged in user' do
+    visit brigade_page_path
+    click_on "Seguir" #Follow
+    expect(page).to have_content 'Estas siguiendo esta brigada ahora'
+    visit user_page_path
+    expect(page).to have_content 'Monterrey'
+  end
+  
+  scenario 'Unfollow a followed Brigade as a logged in user' do
+    visit brigade_page_path
+    click_on "Seguir"
+    visit user_page_path
+    click_on "Dejar de seguir" #Unfollow
+    expect(page).to have_content 'No estas siguiendo Monterrey ahora'
+  end
+  
+  scenario 'Make sure cannot refollow an already followed Brigade' do
+    visit brigade_page_path
+    click_on "Seguir" #Follow
+    visit brigade_page_path
+    expect(page).to have_content "Dejar de seguir"
+    expect(page).not_to have_content "Seguir"
   end
 end
