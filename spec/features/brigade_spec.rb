7710a98db85d881bd600a95ea7bb887edbe406f8
Kyle Boss <kyle@kyleboss.com>
2016-03-22 14:31:01 -0600

    All tests pass for brigade creation with zip-code check

--- spec/features/brigade_spec.rb
+++ spec/features/brigade_spec.rb
@@ -5,15 +5,22 @@ feature 'Brigades' do
     user = new_user
     sign_in_user(user)
   end
+  fixtures :locations
   
   scenario 'Creating a Brigade as a logged in user' do
-    visit new_brigade_en_path
-    expect(page).to have_content I18n.t('brigades.new.header')
-    expect(page).to have_content I18n.t('brigades.new.submit')
-    fill_in 'brigade_location', with: '1'
-    fill_in 'brigade_description', with: 'Bienvenido a la brigada de Monterrey!'
-    click_on I18n.t('brigades.new.submit')
-    expect(page).to have_content 'Monterrey'
+    Capybara.using_driver :selenium do
+      user = new_user
+      sign_in_user(user)
+      visit new_brigade_en_path
+      expect(page).to have_content I18n.t('brigades.new.header')
+      expect(page).to have_content I18n.t('brigades.new.submit')
+      fill_in 'location-query', with: 'tequila'
+      page.should have_selector(".location-list-option")
+      first(".location-list-option").click
+      fill_in 'brigade_description', with: 'Bienvenido a la brigada de Monterrey!'
+      click_on I18n.t('brigades.new.submit')
+      expect(page).to have_content 'Monterrey'
+    end
   end
   
   scenario 'Follow a Brigade as a logged in user' do
