a9f24b7d603ebc212488d2b97e1147f690868bc2
Kyle Boss <kyle@kyleboss.com>
2016-03-22 13:49:11 -0600

    Fixed tests. Added trigram to travis

--- spec/factories/searches.rb
+++ spec/factories/searches.rb
@@ -2,5 +2,6 @@
 
 FactoryGirl.define do
   factory :search do
+
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -6,21 +6,20 @@ describe Location do
     describe 'given a valid zip code' do
       describe 'given the zip code has a locality' do
         it 'should return a hash with state, city, and locality' do
-          expect(Location.search('64000')).to eq [ locations(:location_with_locality) ]
+          expect(Location.search('64000').first.locality).to eq "Col. Centro"
         end
       end
 
       describe 'given the zip code does not have a locality' do
         it 'should return a hash with state, city, but no locality' do
-          expect(Location.search('48400')).to eq [ locations(:location_without_locality) ]
+          expect(Location.search('48400').first.locality).to eq nil
         end
       end
     end
 
-    describe 'given an invalid zip code' do
-      it 'should return an empty hash' do
-        expected_location = []
-        expect(Location.search('48401')).to eq expected_location
+    describe 'given a typod zip code' do
+      it 'should return a fuzzy-searched result with the closest Locations' do
+        expect(Location.search('48401').first.city).to eq("Tequila")
       end
     end
   end
