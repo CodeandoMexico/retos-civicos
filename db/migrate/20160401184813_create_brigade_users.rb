50c8958311e03999cec1e723c3ccd18d6d0dc7bb
jakegilger <jgilger@berkeley.edu>
2016-04-05 07:14:57 +0000

    Added translations and follow button to Brigade UI

---
+++ db/migrate/20160401184813_create_brigade_users.rb
@@ -0,0 +1,12 @@
+class CreateBrigadeUsers < ActiveRecord::Migration
+  def change
+    create_table :brigade_users do |t|
+      t.references :user
+      t.references :brigade
+
+      t.timestamps
+    end
+    add_index :brigade_users, :user_id
+    add_index :brigade_users, :brigade_id
+  end
+end
