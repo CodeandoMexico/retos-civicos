320e68f085435c6dbedb6224883600379a2e6aa1
Kyle Boss <kyle@kyleboss.com>
2016-04-08 09:20:46 -0700

    Added more tests

--- spec/features/step_definitions/brigade_steps.rb
+++ spec/features/step_definitions/brigade_steps.rb
@@ -9,3 +9,11 @@ Given(/^I visit the brigade page for (.+), (.+)$/) do |city, state|
   brigade = Brigade.includes(:location).where(locations: { state: state, city: city }).first
   visit brigade_path(brigade)
 end
+
+Given(/^the following users are in brigade (.+), (.+)$/) do |city, state, table|
+  brigade_id = Brigade.includes(:location).where(locations: { state: state, city: city }).first.id
+  table.hashes.each do |user|
+    user_id = User.create!(user)
+    BrigadeUser.create(user_id: user_id, brigade_id: brigade_id)
+  end
+end
