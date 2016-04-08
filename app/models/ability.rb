089f9fcb03fc34fce933d7e4040d966e8a9e2615
Kyle Boss <kyle@kyleboss.com>
2016-04-08 14:26:30 -0700

    Fixed ability complexity

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,104 +2,142 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    alias_action :read, :update, :mark_valid, :mark_invalid, :publish, :accept, :winner, :remove_winner,
-                 to: :all_entries_actions
     user ||= User.new
+    set_general_permissions(user)
+    set_permissions_for_organizations(user) if user.organization?
+    set_permissions_for_judges(user) if user.judge?
+    set_permissions_for_members(user) if user.member?
+  end
 
-    # Visitor access
+  def set_general_permissions(user)
+    grant_visitor_access(user)
+    grant_entry_read_access(user)
+  end
+
+  def set_permissions_for_organizations(user)
+    grant_challenge_access(user)
+    grant_comment_access(user)
+    grant_organization_access(user)
+    grant_all_entries_access(user)
+    grant_judges_access
+  end
+
+  def set_permissions_for_judges(user)
+    grant_read_challenge_access
+    grant_evaluation_access(user)
+    grant_entries_access(user)
+  end
+
+  def set_permissions_for_members(user)
+    grant_like_challenge_access
+    grant_collaboration_access
+    grant_members_access(user)
+    grant_like_comment_access(user)
+    grant_create_comment_access(user)
+  end
+
+  def grant_visitor_access(user)
     can [:edit, :update, :define_role, :set_role], User do |u|
       user.id == u.id
     end
     can [:read], Organization
+  end
 
-    # Entry read access
+  def grant_entry_read_access(user)
     can :read, Entry do |entry|
       # allow read if the user created the entry
       # or the entry is public
       entry.public? || (user.userable.present? && user.userable.id == entry.member.id)
     end
+  end
 
-    if user.organization?
-      # Challenge access
-      can [:new, :create], Challenge
-      can [:edit, :update, :cancel, :send_newsletter, :mail_newsletter], Challenge do |challenge|
-        challenge.organization.id == user.userable.id
-      end
-
-      can [:manage], Challenge do |challenge|
-        challenge.organization_id == user.userable.id
-      end
-      can [:like], Challenge
-
-      # Comment access
-      can [:like], Comment do |comment|
-        comment.user.id != user.id && !user.voted_on?(comment)
-      end
-
-      # Organization access
-      can [:edit, :update, :subscribers_list, :send_newsletter, :mail_newsletter], Organization do |org|
-        org.id == user.userable.id
-      end
-
-      can [:create, :reply], Comment
-      # Entries access
-      can [:all_entries_actions], Entry do |entry|
-        entry.challenge.organization_id == user.userable.id
-      end
-
-      # Judges access
-      can [:read, :create, :update], Judge
-      can [:manage], Evaluation
-      can [:read], ReportCard
+  def grant_challenge_access(user)
+    can [:new, :create], Challenge
+    can [:edit, :update, :cancel, :send_newsletter, :mail_newsletter], Challenge do |challenge|
+      challenge.organization.id == user.userable.id
     end
 
-    if user.judge?
-      # challenge access
-      can [:read], Challenge
-
-      # evaluation access
-      can [:new, :create, :update], Evaluation do |evaluation|
-        evaluation.judge_id == user.userable.id
-      end
-
-      # Entries access
-      # should this be limited only to evaluation entries?
-      can [:read], Entry
-
-      can [:update], Judge do |judge|
-        judge.id == user.userable.id
-      end
-
-      can [:read, :update], ReportCard do |report_card|
-        report_card.evaluation.judge.id == user.userable.id
-      end
+    can [:manage], Challenge do |challenge|
+      challenge.organization_id == user.userable.id
     end
+    can [:like], Challenge
+  end
 
-    if user.member?
-      # Challenge access
-      can [:like], Challenge
-
-      # Collaboration access
-      can [:create], Collaboration
-
-      # Members access
-      can [:show, :edit, :update], Member do |member|
-        user.userable.id == member.id
-      end
-
-      # Comment access
-      can [:like], Comment do |comment|
-        comment.user.id != user.id && !user.voted_on?(comment)
-      end
-
-      # Comment creation for members, restricting access through challenge
-      can [:create, :reply], Comment
-      can [:create_or_reply_challenge_comment], Challenge
-
-      can [:create], Entry
-      can [:update], Entry do |entry|
-        user.userable.id == entry.member.id
-      end
+  def grant_comment_access(user)
+    can [:like], Comment do |comment|
+      comment.user.id != user.id && !user.voted_on?(comment)
     end
   end
+
+  def grant_organization_access(user)
+    can [:edit, :update, :subscribers_list, :send_newsletter, :mail_newsletter], Organization do |org|
+      org.id == user.userable.id
+    end
+    can [:create, :reply], Comment
+  end
+
+  def grant_all_entries_access(user)
+    alias_action :read, :update, :mark_valid, :mark_invalid, :publish, :accept, :winner, :remove_winner,
+                 to: :all_entries_actions
+    can [:all_entries_actions], Entry do |entry|
+      entry.challenge.organization_id == user.userable.id
+    end
+  end
+
+  def grant_judges_access
+    can [:read, :create, :update], Judge
+    can [:manage], Evaluation
+    can [:read], ReportCard
+  end
+
+  def grant_read_challenge_access
+    can [:read], Challenge
+  end
+
+  def grant_evaluation_access(user)
+    can [:new, :create, :update], Evaluation do |evaluation|
+      evaluation.judge_id == user.userable.id
+    end
+  end
+
+  def grant_entries_access(user)
+    can [:read], Entry
+    can [:update], Judge do |judge|
+      judge.id == user.userable.id
+    end
+    can [:read, :update], ReportCard do |report_card|
+      report_card.evaluation.judge.id == user.userable.id
+    end
+  end
+
+  def grant_like_challenge_access
+    can [:like], Challenge
+  end
+
+  def grant_collaboration_access
+    can [:create], Collaboration
+  end
+
+  def grant_members_access(user)
+    can [:show, :edit, :update], Member do |member|
+      user.userable.id == member.id
+    end
+  end
+
+  def grant_like_comment_access(user)
+    can [:like], Comment do |comment|
+      comment.user.id != user.id && !user.voted_on?(comment)
+    end
+  end
+
+  def grant_create_comment_access(user)
+    can [:create, :reply], Comment
+    can [:create_or_reply_challenge_comment], Challenge
+
+    can [:create], Entry
+    can [:update], Entry do |entry|
+      user.userable.id == entry.member.id
+    end
+  end
+
 end
