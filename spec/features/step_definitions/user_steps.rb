5a9bd5bf292afb1aff7a8a2665387b3e2865267d
jakegilger <jgilger@berkeley.edu>
2016-04-14 09:08:23 +0000

    Fixed brigade_has_show_view cucumber test. Now my button feature is active.

--- spec/features/step_definitions/user_steps.rb
+++ spec/features/step_definitions/user_steps.rb
@@ -5,7 +5,7 @@ Given(/^the following users exist:$/) do |table|
 end
 
 Given /^I am logged in as a user$/ do
-  @current_user = User.create!(email: 'test2@test.com', password: "111111")
+  @current_user = User.create!(email: 'test0@test.com', password: "111111")
   login_as(@current_user, :scope => :user)
 end
 
