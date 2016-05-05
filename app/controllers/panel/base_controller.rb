f278e3534c6f691edd89be299618f6ab8cbb83c2
Adrian Rangel <adrian.rangel@gmail.com>
2016-05-05 08:40:05 -0500

    use guard clauses instead of if blocks

--- app/controllers/panel/base_controller.rb
+++ app/controllers/panel/base_controller.rb
@@ -2,9 +2,8 @@ module Panel
   class BaseController < ApplicationController
     add_crumb 'Panel', '/panel/entradas'
     def authenticate_member!
-      unless current_user && current_user.member?
-        redirect_back_or challenges_path, I18n.t('flash.unauthorized.message')
-      end
+      return if current_user && current_user.member?
+      redirect_back_or challenges_path, I18n.t('flash.unauthorized.message')
     end
   end
 end
