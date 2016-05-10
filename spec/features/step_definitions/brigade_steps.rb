eb632a77759de0be2d61672f5124080604dacd36
Kyle Boss <kyle@kyleboss.com>
2016-05-10 18:07:13 -0500

    Testing purposes

--- spec/features/step_definitions/brigade_steps.rb
+++ spec/features/step_definitions/brigade_steps.rb
@@ -65,6 +65,6 @@ def wait_for_ajax
   while page.execute_script("return $.active").to_i > 0
     counter += 1
     sleep(0.1)
-    raise "AJAX request took longer than 5 seconds." if counter >= 50
+    raise "AJAX request took too long than 5 seconds." if counter >= 50
   end
 end
