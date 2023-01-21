8a765f62eabb3bdbf1edfdae65164c073520166f
Abraham Toriz <categulario@gmail.com>
2023-01-21 12:58:15 -0600

    syntax error in scope

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ActiveRecord::Base
   scope :for_voter, ->(*args) { where(['voter_id = ? AND voter_type = ?', args.first.id, args.first.class.base_class.name]) }
   scope :for_voteable, ->(*args) { where(['voteable_id = ? AND voteable_type = ?', args.first.id, args.first.class.base_class.name]) }
   scope :recent, ->(*args) { where(['created_at > ?', (args.first || 2.weeks.ago)]) }
-  scope :descending, order('created_at DESC')
+  scope :descending, -> { order('created_at DESC') }
 
   belongs_to :voteable, polymorphic: true
   belongs_to :voter, polymorphic: true
